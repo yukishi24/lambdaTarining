5a87072cec191de6c44b1b8257f87597f7d7cc0c
yukishi24 <yukishi24github@gmail.com>
2022-12-22 23:17:31 +0900

    lambdaのケース

---
+++ src/main/java/lambda/InterfaceTest.java
@@ -0,0 +1,13 @@
+package lambda;
+
+public interface InterfaceTest {
+  // 引数なし
+  // void method();
+
+  // 引数あり
+  // void print(String string);
+
+  // 戻り値あり
+  //  public int rmethod();
+
+}
---
+++ src/main/java/lambda/Main.java
@@ -0,0 +1,19 @@
+package lambda;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // 引数なし
+    // InterfaceTest it = () -> System.out.println("Hello World!!!");
+    // it.method();
+
+    // 引数あり
+    // InterfaceTest it = a -> System.out.println(a.length());
+    // it.print("test");
+
+    // 戻り値あり
+    // InterfaceTest it = () -> {return 1;};
+    // System.out.println(it.rmethod());
+  }
+
+}
